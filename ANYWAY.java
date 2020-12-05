1c64b565f13a5c4368d220b0ef6f47f2374b8be9
preshak <preshakvarshney2018@gmail.com>
2020-12-05 16:00:14 +0530

    addd

---
+++ ANYWAY.java
@@ -0,0 +1,12 @@
+ class ANYWAY extends Thread{
+    public void run(){
+      System.out.println(Thread.currentThread().getName()) ; 
+    }
+public static void main(String ar[]){
+    a obj=new a();
+    a obj1=new a();
+    obj.setPriority(Thread.MAX_PRIORITY);
+    obj1.setPriority(Thread.MIN_PRIORITY);
+    obj.start();
+    obj1.start();
+}}
