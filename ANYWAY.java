6ddce76082963937fb9474bb0a9cd8766a7e0bb1
preshak <preshakvarshney2018@gmail.com>
2020-12-05 23:06:46 +0530

    asdf

--- ANYWAY.java
+++ ANYWAY.java
@@ -1,6 +1,7 @@
  class ANYWAY extends Thread{
     public void run(){
       System.out.println(Thread.currentThread().getName()) ; 
+System.out.println("do");
     }
 public static void main(String ar[]){
     a obj=new a();
